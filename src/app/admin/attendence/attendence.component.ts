60967ace006306e1a2a5085db4666adcb4d24270
Pratibha <pratibhasutar6451@gmail.com>
2023-05-16 12:15:26 +0530

    commit

--- src/app/admin/attendence/attendence.component.ts
+++ src/app/admin/attendence/attendence.component.ts
@@ -26,21 +26,18 @@ employee_detail:any;
 employee_details:any;
 present:any;
 absent:any;
-// attendence:number =0;
-
+selectedDate:any;
 startdate:any;
 enddate:any;
 filterAttendence:any;
+filteredPresentList:any;
 total:any;
 constructor(private api:ApiService){
 
 }
 ngOnInit(): void {
-  // this.api.get("employee_details/list").subscribe((result:any)=>{
-  //   this.employee_details = result.data;
-    
-  //   });
-  
+ 
+  this.bind();
   this.api.get("category/list").subscribe((result:any)=>{
     this.categories = result.data;
     
@@ -57,8 +54,6 @@ edit(id:string){
         {
           id:new FormControl(""),
         category_id:new FormControl("", Validators.required),
-        // employee_details_id:new FormControl("", Validators.required),
-        // date:new FormControl("", Validators.required),
         startdate: new FormControl((new Date()).toISOString().substring(0, 10)),
         enddate:new FormControl((new Date()).toISOString().substring(0, 10)),
         present:new FormControl("", Validators.required),
@@ -73,29 +68,18 @@ edit(id:string){
       console.log(result);
       this.attendences = result.data;
       
-      this.filterAttendence = result.data;
-      // this.filterAttendence.forEach(this.filterAttendence => {
-      // this.total += parseInt(this.filterAttendence.present)
-      //   });
+      this.filteredPresentList = result.data;
+     
       
     });
     
-    // this.api.get("attendences/list").subscribe((result: any) => {
-    //   //console.log(result);
-    //   this.attendences = result.data;
-    //   this.filterAttendence = result.data;
-    //   // this.attendences.forEach(this.attendences => {
-    //   //   this.attendence += parseInt(this.attendences.present)
-    //   // });
-    //  console.log(result);
-    // });
+  
 
     this.formdata = new FormGroup(
       {
         id:new FormControl(""),
         category_id:new FormControl("", Validators.required),
-        // employee_details_id:new FormControl("", Validators.required),
-        // date:new FormControl("", Validators.required),
+       date:new FormControl("", Validators.required),
         startdate: new FormControl((new Date()).toISOString().substring(0, 10)),
         enddate:new FormControl((new Date()).toISOString().substring(0, 10)),
         attendence:new FormControl("", Validators.required),
@@ -115,19 +99,25 @@ edit(id:string){
     }
   }
 
-// onDateSelected() {
-// debugger
-//    let startDate = +new Date(this.startdate);
-//      let endDate = +new Date(this.enddate);
-//     let result = this.attendences.filter((data: any) => {
-//        return + new Date(data.startdate) >= startDate && +new Date(data.enddate) <= endDate;
-//     })
-//     this.filterAttendence = result;
-//     console.log(result);
-//     // this.filterAttendence = result.data;
-//      this.show = !this.show;
-//     }
+onDateSelected() {
+let presentCount = 0;
+let absentCount = 0;
+let startDate = + new Date(this.startdate);
+let endDate = + new Date(this.enddate);  
+    let result = this.attendences.filter((presentList:any)=>{
+      if(presentList.present == "1")
+      return true;
 
-  
+      return + new Date(presentList.startdate) >= startDate && +new Date(presentList.startdate) <= endDate;
 
+      
+
+  });
+  this.filteredPresentList = result;
+    console.log(result);
+   
+    this.show = !this.show;
+
+  }
 }
+
